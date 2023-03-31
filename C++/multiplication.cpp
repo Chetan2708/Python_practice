e1060e6961b55186ea259152a214c8b05c78aa8a
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-03-31 23:50:34 +0530

    Add files via upload

---
+++ C++/multiplication.cpp
@@ -0,0 +1,54 @@
+
+
+ 
+/*#include<iostream>
+using namespace std;
+int main()
+{
+int i,  n ;
+cout<<"enter what number's table you want";
+cin>>n;
+ cout<<"enter the limit";
+ cin>>i;
+
+while (i>=1)
+{
+    
+    cout<<i<<"*"<<n<<"="<<i*n<<endl;
+i--;
+}
+
+
+
+return 0;
+
+}*/
+
+/*#include<iostream>
+using namespace std;
+int main()
+{
+
+for (int i =40; i >=4; i-=4)
+{
+    cout<<i<<endl;
+    
+}
+return 0;
+}*/
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,  n ;
+cout<<"enter what number's table you want";
+cin>>n;
+
+for (int i = 10; i >=1; i--)
+{
+    cout<<i<<"*"<<n<<"="<<i*n<<endl;
+}
+}
+
