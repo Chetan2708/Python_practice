0a4d4f8a7c078d66d2624f49c81e50f1eb3df6a3
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-04-11 22:05:12 +0530

    Add files via upload

---
+++ overloading.py
@@ -0,0 +1,16 @@
+class complex:
+    def __init__(self,re , im):
+        self.real = re
+        self.imag = im
+    
+    def __add__(self,other):
+        return self.real + other.real , self.imag + other.imag
+    def display(self):
+        print(self.real ,"+", self.imag,"j")
+    
+obj = complex(5,8)
+obj2= complex(2,3)
+obj.display()
+obj2.display()
+c,d=obj+obj2 
+print(c,"+",d,"j")
