1d2cd0fea8a6b679a591acf9cefd756bd6561359
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-04-13 22:26:00 +0530

    Add files via upload

---
+++ Pattern/Pattern3.py
@@ -0,0 +1,12 @@
+n= 5 
+for i in range(0, n):
+    for j in range(0,n-i):
+        print("*", end =" ")
+    print()
+
+
+#          * * * * * 
+#          * * * * 
+#          * * * 
+#          * * 
+#          *     
