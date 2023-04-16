867eb92839fe5e3904a963323dfa0ee354d6bb95
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-04-16 23:06:39 +0530

    Add files via upload

---
+++ Pattern/Pattern11.py
@@ -0,0 +1,27 @@
+n = 5
+for i in range(n-1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(1,i+1):
+        print("*",end=" ")    
+    for f in range(i+1):
+        print("*",end=" ")  
+    print()
+for i in range(n):
+    for k in range(i+1):   # 2nd line space starting 
+        print(" ",end=" ")  
+    for j in range(n-i):
+        print("*",end=" ")    # 1-5 printing 1st line
+    for k in range(1,n-i):   # 1-4
+        print("*",end=" ")  
+    print()
+
+#           * 
+#         * * * 
+#       * * * * * 
+#     * * * * * * * 
+#   * * * * * * * * * 
+#     * * * * * * * 
+#       * * * * * 
+#         * * * 
+#           * 
